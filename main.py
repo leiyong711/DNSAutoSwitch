084736e3fd6d9de56a05cb6cefec72aefe9bdc38
leiyong <yong.lei@imaygou.com>
2022-08-11 15:22:19 +0800

    新增自动重启路由器接口

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ class DNSAutoSwitch:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
             'X-Requested-With': 'XMLHttpRequest'
         }
+        self.login_status = False
+        self.login()
 
     def encrypt_passwd(self, a, b, c):
         """
@@ -68,16 +70,20 @@ class DNSAutoSwitch:
         登陆后台
         :return:
         """
-        params = {
-            "method": "do",
-            "login": {
-                "password": self.password
+        try:
+            params = {
+                "method": "do",
+                "login": {
+                    "password": self.password
+                }
             }
-        }
 
-        resp = requests.post(url=self.url, headers=self.headers, json=params).json()
-        if resp.get('error_code') == 0 and resp.get('stok'):
-            self.stok = resp.get('stok')
+            resp = requests.post(url=self.url, headers=self.headers, json=params).json()
+            if resp.get('error_code') == 0 and resp.get('stok'):
+                self.stok = resp.get('stok')
+                self.login_status = True
+        except:
+            lg.warning(f"路由器登陆异常,原因\n: {traceback.format_exc()}")
 
     def ping(self, ip, port=80, verifi_count=3, time_out=5):
         """
@@ -112,14 +118,14 @@ class DNSAutoSwitch:
         """
         path = f'/stok={self.stok}/ds'
         params = {
-            "hyfi":{
+            "hyfi": {
                 "table": ["connected_ext"]
             },
             "hosts_info": {
-                "table":"online_host",
-                "name":"cap_host_num"
+                "table": "online_host",
+                "name": "cap_host_num"
             },
-            "method":"get"
+            "method": "get"
         }
         try:
             resp = requests.post(url=self.url + path, headers=self.headers, json=params).json()
@@ -168,6 +174,27 @@ class DNSAutoSwitch:
             lg.error(f"获取当前网关异常,原因:\n{traceback.format_exc()}")
             return {}
 
+    def reboot(self):
+        """
+        重启路由器
+        :return:
+        """
+        path = f'/stok={self.stok}/ds'
+        params = {
+            "system": {
+                "reboot": None
+            },
+            "method": "do"
+        }
+        try:
+            resp = requests.post(url=self.url + path, headers=self.headers, json=params).json()
+            if resp.get('error_code') == 0 and resp.get('wait_time'):
+                lg.info(f"正在重启路由器, 请稍等 {resp.get('wait_time')} s ...")
+            else:
+                lg.error(f"重启路由器失败,响应:\n{str(resp)}")
+        except:
+            lg.error(f"重启路由器异常,原因:\n{traceback.format_exc()}")
+
     def set_dns(self, ip):
         """
         设置DNS、网关
@@ -182,13 +209,13 @@ class DNSAutoSwitch:
         params = {
             "dhcpd": {
                 "udhcpd": {
-                    "auto": int(udhcpd.get('auto'), '1'),
+                    "auto": int(udhcpd.get('auto', '1')),
                     "pool_start": udhcpd.get('pool_start'),
                     "pool_end": udhcpd.get('pool_end'),
-                    "lease_time": int(udhcpd.get('lease_time'), '7200'),
+                    "lease_time": int(udhcpd.get('lease_time', '7200')),
                     "gateway": ip,  # 网关
                     "pri_dns": ip,  # 首选DNS
-                    "snd_dns": ip   # 备选DNS
+                    "snd_dns": udhcpd.get('snd_dns')  # 备选DNS
                 }
             },
             "method": "set"
@@ -203,15 +230,23 @@ class DNSAutoSwitch:
             lg.error(f"修改DNS、网关为 {ip} 异常,原因:\n{traceback.format_exc()}")
 
     def main(self, open_wrt_ip, default_gateway='0.0.0.0'):
+
+        if not self.login_status:
+            lg.warning("路由器登陆失败, 可能正在进行重启")
+            return
+
         online_host = self.ip_status()
         open_wrt_status = False  # OpenWRT服务可用状态
+        update_text = ''  # OpenWRT服务可用状态
         for ip_info in online_host:
-            if open_wrt_ip in ip_info.get('ip'):
 
-                # 获取 OpenWRT IP 延迟
-                average = float(self.ping(open_wrt_ip, port=80, verifi_count=3, time_out=5))
-                if average != 0.0 and average < 100:
-                    open_wrt_status = True
+            for k, v in ip_info.items():
+                if open_wrt_ip in v.get('ip'):
+                    # 获取 OpenWRT IP 延迟
+                    average = float(self.ping(open_wrt_ip, port=80, verifi_count=3, time_out=10))
+                    if average != 0.0 or average <= 100:
+                        update_text = f"OpenWRT IP延迟: {average} ms, 符合阈值 1 - 100ms 范围"
+                        open_wrt_status = True
 
         udhcpd = self.get_dns_info()
         if udhcpd:
@@ -219,15 +254,22 @@ class DNSAutoSwitch:
             if open_wrt_status and open_wrt_ip not in udhcpd.get('gateway'):
                 # 修改网关到OpenWRT
                 lg.info(f"正在修改网关到OpenWRT...")
+                lg.info(f"修改原因: {update_text}")
                 self.set_dns(open_wrt_ip)
                 lg.info(f"修改网关到OpenWRT成功")
+                self.reboot()  # 重启路由器
 
             # OpenWRT服务异常 且 当前路由器网关 为OpenWRT IP
             elif not open_wrt_status and open_wrt_ip in udhcpd.get('gateway'):
                 # 还原默认网关
                 lg.info(f"正在还原默认网关...")
+                lg.info(f"修改原因: 未从路由器找到OpenWRT IP")
                 self.set_dns(default_gateway)
                 lg.info(f"还原默认网关成功")
+                self.reboot()  # 重启路由器
+
+            else:
+                lg.info(f"无需修改")
 
 
 if __name__ == '__main__':
